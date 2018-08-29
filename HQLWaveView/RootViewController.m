87c8dde362ec15fc017f5f6ef472ef4a9732ace9
HQL-yunyunyun <he84965092@163.com>
2018-08-29 15:30:52 +0800

    commit

--- HQLWaveView/RootViewController.m
+++ HQLWaveView/RootViewController.m
@@ -10,28 +10,18 @@
 
 @interface RootViewController ()
 
+@property (nonatomic, strong) CAShapeLayer *shapeLayer;
+@property (nonatomic, assign) NSInteger count;
+
 @end
 
 @implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onButtonClick:(id)sender {
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
